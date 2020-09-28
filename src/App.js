982838b4b5caf70e05d75106b186115dd5c26257
Denys <pugachovdenys@gmail.com>
2020-09-28 12:20:28 +0300

    end 4 Main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Navbar } from './components/Navbar';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Profile } from './pages/Profile';
 
 function App() {
     return (
-        <div className="container pt-4">
-            <h1>Hello React Hooks</h1>
+        <BrowserRouter>
+            <Navbar />
+            <div className="container pt-4">
+                <Switch>
 
-        </div>
+                    <Route path="/" exact component={ Home } />
+                    <Route path="/about" component={ About } />
+                    <Route path="/profile/:name" component={ Profile } />
+                </Switch>
+
+            </div>
+        </BrowserRouter>
     );
 }
 
